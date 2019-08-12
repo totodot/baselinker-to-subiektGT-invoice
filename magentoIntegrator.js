8dd228240992233d164a930340104ee312246184
totodot <toto.zetka@gmail.com>
2019-08-12 20:44:29 +0200

    RestApi

--- magentoIntegrator.js
+++ magentoIntegrator.js
@@ -1,42 +1,30 @@
-// require('colors');
-const sql = require('mssql');
+require('colors');
 const { subiektGT: subiektGTConfig } = require('./config');
-// const SubiektGT = require('./modules/Subiekt');
-// const MagentoIntegrator = require('./modules/MagentoIntegrator');
-// const mapping = require('./maps/baselinkerMap');
-// const Logger = require('./utils/loggerUtil');
+const MsSql = require('./modules/MsSql');
+const MagentoRun = require('./modules/MagentoIntegrator');
+const Logger = require('./utils/loggerUtil');
 
-// const GT = new SubiektGT(subiektGTConfig);
-
-// try {
-//   Logger.info('Połączenie do SUBIEKT GT');
-//   const GtInstance = GT.connect();
-//   // const Magento = new MagentoIntegrator({}, GtInstance);
-// } catch (e) {
-//   Logger.info('Błąd połączenia do SUBIEKT GT');
-// }
-const {
-  user, pass, server, dbName,
-} = subiektGTConfig;
-
-const connect = async () => {
+const run = async () => {
   try {
-    const url = `mssql://${user}:${pass}@${server}/${dbName}`;
-    console.log(url);
-    await sql.connect(url);
-    const result = await sql.query`select tw_Symbol as code,
-                                  tw_Id as id, st_Stan as stan,
-                                  tw_Nazwa as nazwa,
-                                  tc_CenaBrutto1 as brutto1,
-                                  tc_CenaBrutto2 as brutto2
-                                  FROM vwTowar WHERE tw_SklepInternet = 1`;
-    console.log(result);
-
-    sql.close();
+    await MsSql.connect(subiektGTConfig);
+    const groupId = await MsSql.getGroup('GREENLUX');
+    const result = await MsSql.query(`select tw_Symbol as sku,
+                              tw_Id as id, st_Stan as qty,
+                              tw_Nazwa as name,
+                              tc_CenaBrutto4 as price,
+                              tc_CenaBrutto5 as bruttoAllegro
+                              FROM vwTowar WHERE tw_SklepInternet = 1
+                              AND tw_IdGrupa = ${groupId}`);
+    MagentoRun(result);
   } catch (err) {
-    console.log(err);
-    // ... error checks
+    console.error(err);
   }
 };
 
-connect();
+run()
+  .then(() => {
+    MsSql.close();
+  })
+  .catch(() => {
+    MsSql.close();
+  });
--- modules/MagentoIntegrator.js
+++ modules/MagentoIntegrator.js
@@ -1,17 +1,147 @@
-const Logger = require('../utils/loggerUtil');
+const axios = require('axios');
+const {
+  magento: { token, attribute_set_id, category_id },
+} = require('../config');
 
-class MagentoIntegrator {
-  constructor(config, GT) {
-    Logger.info('MagentoInegrator Instacne created!');
-    this.config = config;
-    this.GT = GT;
+axios.defaults.baseURL = 'http://store.swiecisie.pl/rest/V1';
+axios.defaults.headers.common = { Authorization: `bearer ${token}` };
+
+const getProduct = async (sku) => {
+  try {
+    const result = await axios.get(`/products/${sku}`);
+    return result.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const postProduct = async (data) => {
+  try {
+    const result = await axios.post('/products', { product: data });
+    return result.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const createProduct = async ({
+  name, sku, price, qty,
+}) => {
+  try {
+    const result = await postProduct({
+      name,
+      sku,
+      price,
+      attribute_set_id,
+      status: 0,
+      visibility: 4,
+      type_id: 'simple',
+      extension_attributes: {
+        category_links: [
+          {
+            position: 0,
+            category_id,
+          },
+        ],
+        stock_item: {
+          qty,
+          is_in_stock: qty !== 0,
+        },
+      },
+    });
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const updateProduct = async ({
+  name, sku, price, qty,
+}) => {
+  try {
+    const result = await postProduct({
+      name,
+      sku,
+      price,
+      extension_attributes: {
+        stock_item: {
+          qty,
+          is_in_stock: qty !== 0,
+        },
+      },
+    });
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const needUpdate = (product, data) => {
+  if (Number(product.price) !== Number(data.price)) {
+    return true;
+  }
+  if (Number(product.qty) !== Number(data.extension_attributes.stock_item.qty)) {
+    return true;
   }
 
-  run() {
-    console.log(this.config);
+  return false;
+};
+
+const run = async (products) => {
+  const start = new Date();
+  console.log(`Find ${products.length} products`);
+  const items = {
+    toCreate: 0,
+    created: 0,
+    toUpdate: 0,
+    updated: 0,
+  };
+  for (productIndex in products) {
+    console.log(`${Number(productIndex) + 1}/${products.length}`);
+    const product = products[productIndex];
+    let data = null;
+    let error = null;
+    try {
+      data = await getProduct(product.sku);
+    } catch (err) {
+      if (err.response.status === 404) {
+        console.log(err);
+        console.log('Not item found');
+      } else {
+        console.log(err);
+        error = err;
+      }
+    }
+
+    if (error) {
+      continue;
+    }
+
+    if (data && needUpdate(product, data)) {
+      items.toUpdate++;
+      try {
+        await updateProduct(product);
+        items.updated++;
+      } catch (err) {
+        console.log('Cannot update product');
+      }
+    }
+
+    if (!data) {
+      items.toCreate++;
+      try {
+        await createProduct(product);
+        items.created++;
+      } catch (err) {
+        console.log('Cannot create product');
+      }
+    }
   }
-}
 
-MagentoIntegrator.instance = null;
+  console.log(`Created ${items.created}/${items.toCreate}`);
+  console.log(`Updated ${items.updated}/${items.toUpdate}`);
+  const end = new Date() - start;
+  console.info('Execution time: %ds', end / 1000);
+};
 
-module.exports = MagentoIntegrator;
+module.exports = run;
